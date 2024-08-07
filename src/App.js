b80d5b9cf734e503ec7fc3a712b9d14dba1a28c9
Agata Kamińska <a.fluffypie@gmail.com>
2024-08-07 21:18:16 +0200

    feat: created Footer component, css styles included

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Section from "./components/Section/Section";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return <>
   <Header />
   <Section />
+  <Footer />
   </>;
 }
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import "./Footer.css";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faFacebook } from "@fortawesome/free-brands-svg-icons";
+
+const Footer = () => {
+  return (
+    <div className="flex j-c--center a-i--center footer">
+      <div className="flex a-i--center footer--direction-and-justify container">
+        <p className="paragraph bold footer__paragraph">
+          Nazwa firmy - wszelkie prawa zastrzeżone, 2024
+        </p>
+        <div className="flex j-c--center a-i--center">
+          <a
+            href="https://www.instagram.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faInstagram} size="2x" className="icon" />
+          </a>
+          <a
+            href="https://www.facebook.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faFacebook} size="2x" className="icon" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
