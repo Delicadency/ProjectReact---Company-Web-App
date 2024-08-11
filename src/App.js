914ff4125adc59d8724eb15088603515342dbc3a
Agata Kamińska <a.fluffypie@gmail.com>
2024-08-11 19:49:10 +0200

    fix: changed the way the App component is created from a classic function to an arrow function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from "./components/Header/Header";
 import Section from "./components/Section/Section";
 import Footer from "./components/Footer/Footer";
 
-function App() {
-  return <>
-  <Header />
-  <Section />
-  <Footer />
-  </>;
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Section />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
